d06b69d942e5eb84e21fa7dbc22f42146c2e6739
Amita-Roy <sardana.amita2@gmail.com>
2021-02-10 11:50:34 +0100

    Add weather png

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style/style.scss';
-import cloudSun from './assets/icons/cloudSun.svg';
+// import cloudSun from './assets/icons/cloudSun.svg';
+import sunCloud from './assets/icons/sunCloud.png';
 
 const tempIconContainer = document.querySelector('.temp-icon');
 
 const tempLogo = new Image();
 
-tempLogo.src = cloudSun;
+tempLogo.src = sunCloud;
 
 tempIconContainer.appendChild(tempLogo);
