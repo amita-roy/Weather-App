567127f602617acd187a63f6078d0fb77be6c335
Amita-Roy <sardana.amita2@gmail.com>
2021-02-10 11:27:54 +0100

    Add dummy content for current temp section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style/style.scss';
+import cloudSun from './assets/icons/cloudSun.svg';
+
+const tempIconContainer = document.querySelector('.temp-icon');
+
+const tempLogo = new Image();
+
+tempLogo.src = cloudSun;
+
+tempIconContainer.appendChild(tempLogo);
